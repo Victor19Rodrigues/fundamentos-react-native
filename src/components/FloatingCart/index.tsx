941aa25424a533d19fd99add7dc1e8512e44f5e1
Victor Rodrigues <victor.rodrigues@somospi.com.br>
2020-05-06 16:38:23 -0300

    Finished

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,24 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((total, product) => {
+      const { price, quantity } = product;
 
-    return formatValue(0);
+      total += price * quantity;
+
+      return total;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalItems = products.reduce((total, product) => {
+      total += product.quantity;
+      return total;
+    }, 0);
 
-    return 0;
+    return totalItems;
   }, [products]);
 
   return (
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,33 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalCart = products.reduce((total, product) => {
+      const { price, quantity } = product;
 
-    return formatValue(0);
+      total += price * quantity;
+
+      return total;
+    }, 0);
+
+    return formatValue(totalCart);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalItems = products.reduce((total, product) => {
+      total += product.quantity;
 
-    return 0;
+      return total;
+    }, 0);
+
+    return totalItems;
   }, [products]);
 
   return (
